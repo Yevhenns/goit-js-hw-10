cc8cc87252327f5597a8bb5497133bd9af34a067
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-27 19:54:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import Notiflix from 'notiflix';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const inp = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
