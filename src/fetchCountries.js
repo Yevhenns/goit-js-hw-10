d56e5b5032353b7b51dc1ded75204f6c621c220b
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-28 20:33:21 +0300

    xbxc

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+function fetchCountries(name) {
+  fetch('https://restcountries.com/v3.1/name/#{name}')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Data handling
+    })
+    .catch(error => {
+      // Error handling
+    });
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import Notiflix from 'notiflix';
 import './css/styles.css';
+// import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
 const inp = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
+inp.addEventListener('input', fetchCountries);
+function fetchCountries(name) {
+  const coun = inp.value;
+  fetch('https://restcountries.com/v3.1/name/${coun}')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Data handling
+    })
+    .catch(error => {
+      // Error handling
+    });
+}
+
+// fetchCountries();
